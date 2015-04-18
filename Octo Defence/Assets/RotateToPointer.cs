21aa1fee26b517c18d5f3b0a807b088ccdd2aab8
Fabian Wetzel <fabsenet@googlemail.com>
2015-04-18 06:10:19 +0200

    the connon rotates to the mousepointer

---
+++ Octo Defence/Assets/RotateToPointer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class RotateToPointer : MonoBehaviour {
+	private Camera _cam;
+
+	// Use this for initialization
+	void Start ()
+	{
+		_cam = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		var origin = transform.position;
+		var target = _cam.ScreenToWorldPoint(Input.mousePosition);
+		target = new Vector3(target.x,target.y, origin.z);
+
+		Debug.DrawLine(origin,target);
+
+		var newRotation = Quaternion.LookRotation(_cam.transform.forward, target-origin);
+		transform.rotation = newRotation;
+	}
+}
