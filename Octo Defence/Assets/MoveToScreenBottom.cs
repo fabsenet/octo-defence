1c6c564ff62eb401e604e35582d14a604e6cde68
Fabian Wetzel <fabsenet@googlemail.com>
2015-04-18 05:21:31 +0200

    added background and sticking to bottom

---
+++ Octo Defence/Assets/MoveToScreenBottom.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveToScreenBottom : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+		var cam = Camera.main;
+		var worldPos = cam.ScreenToWorldPoint(new Vector3(Screen.width/2f, 0, 0));
+		transform.position =new Vector3(transform.position.x,worldPos.y, transform.position.z);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
