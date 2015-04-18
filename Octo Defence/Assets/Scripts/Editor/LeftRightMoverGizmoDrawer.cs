f3532474418641b3f441283963f559eee531b19c
Fabian Wetzel <fabsenet@googlemail.com>
2015-04-18 12:16:49 +0200

    exports

---
+++ Octo Defence/Assets/Scripts/Editor/LeftRightMoverGizmoDrawer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class LeftRightMoverGizmoDrawer
+{
+	[DrawGizmo(GizmoType.Active | GizmoType.Selected | GizmoType.NotSelected)]
+	public static void DrawGizmoForLeftRightMover(LeftRightMover src, GizmoType gizmoType)
+	{
+		if (src.StartDirection == LeftRightMover.MovingDirection.Left && !Application.isPlaying)
+		{
+			var movedDistance = src.HorizontalDistance;
+			Debug.DrawLine(new Vector3(src.transform.position.x - movedDistance, src.transform.position.y)
+				, new Vector3(src.transform.position.x - movedDistance + src.HorizontalDistance, src.transform.position.y), Color.yellow);
+		}
+		else
+		{
+			Debug.DrawLine(new Vector3(src.transform.position.x - src._movedDistance, src.transform.position.y)
+				, new Vector3(src.transform.position.x - src._movedDistance + src.HorizontalDistance, src.transform.position.y), Color.yellow);
+		}
+	}
+}
