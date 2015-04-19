b5749874e66541aefbb18cfe9ee09c0a62bdfb1b
Fabian Wetzel <fabsenet@googlemail.com>
2015-04-19 09:32:14 +0200

    mastered aspect ratio; added ship emitter

--- Octo Defence/Assets/AlienShipEmitter.cs
+++ Octo Defence/Assets/AlienShipEmitter.cs
@@ -11,17 +11,26 @@ public class AlienShipEmitter : MonoBehaviour
 
 	private float _nextEmittingTime;
 
+	private float _top, _left, _right, _bottom;
 	// Use this for initialization
 	void Start ()
 	{
 		_nextEmittingTime = Time.time + 1/EmittingRate;
+
+		var center = transform.position;
+
+		_top = center.y + Height/2;
+		_bottom = center.y - Height/2;
+		_left = center.x - Width/2;
+		_right= center.x + Width/2;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Time.time > _nextEmittingTime)
 		{
-			
+			_nextEmittingTime = Time.time + 1/EmittingRate;
+			Instantiate(AlienShipPrefab, new Vector3(Random.Range(_left, _right), Random.Range(_bottom, _top)), Quaternion.identity);
 		}
 	}
 }
---
+++ Octo Defence/Assets/AspectRatioEnforcer.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AspectRatioEnforcer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+		//this game requires at least 16:10 widescreen. 16:9 and 3:2 will just see more left to right.
+		var cam = Camera.main;
+		var aspectRatio = Screen.width/(float)Screen.height;
+
+		if (aspectRatio < 16f / 10f)
+		{
+			//this screen is not wide enough to show 16:10 minimum
+			//we need to apply letterboxing
+			Debug.Log("Aspect ratio " + aspectRatio + " is NOT good enough");
+
+			var desiredScreenHeight = Screen.width/16f*10f;
+
+			var verticalQuotient = desiredScreenHeight/Screen.height;
+			var relativeBarHeight = 1f - verticalQuotient;
+			cam.rect = new Rect(0, relativeBarHeight/2, 1, verticalQuotient);
+		}
+		else
+		{
+			Debug.Log("Aspect ratio "+ aspectRatio+" is good enough");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
